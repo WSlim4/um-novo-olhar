53d655db514ec99c1c94834db308b839cdd4d1a7
Wesley Lima <wesleyjoga500@gmail.com>
2020-05-19 10:53:34 -0300

    + coluna token

--- backend/database/migrations/1503250034279_user.js
+++ backend/database/migrations/1503250034279_user.js
@@ -10,6 +10,9 @@ class UserSchema extends Schema {
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.boolean('is_admin').default(false)
+      table.string('token')
+      table.timestamp('token_created_at')
       table.timestamps()
     })
   }
